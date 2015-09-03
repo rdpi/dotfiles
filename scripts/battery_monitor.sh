18288b9630f2589b7c66c73f9e652eaa25eab329
squanchie <ryanpitts95@gmail.com>
2015-09-03 01:05:27 -0300

    added scripts

---
+++ scripts/battery_monitor.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+while true
+do
+    state=` acpi | awk '{print $3}'`
+    charge=`acpi | grep -P -o '[0-9]+(?=%)'`        
+
+    if [ $state = "Discharging," ] 
+    then
+        if [ $charge -le 20 ] 
+        then
+            notify-send "Battery low, ${charge}% remaining"
+        fi
+    else
+        if [ $state = "Full," ] 
+        then
+            notify-send "Battery Full"
+        fi
+    fi
+    sleep 180
+done
+
